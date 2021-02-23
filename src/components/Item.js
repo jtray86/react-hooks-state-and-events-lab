f589b3ce3dbe213d8a0d7e0f0bfc044f90f5a2f4
jennifer tracy <jenniferetracy@gmail.com>
2021-02-23 17:06:39 -0500

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import React from "react";
 
 function Item({ name, category }) {
+  const [items, setItems] = useState(false);
+
+  function handleItems(){
+    setItems((items)=> !items)
+  }
   return (
-    <li className="">
+    <li className={items ? "in-cart" : ""}
+    >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {handleItems}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [category, setCategory] = useState("All")
+
+  function handleCategory(e){
+    setCategory(e.target.value)}
+    
+    
+  const displayItem = items.filter((item) => {
+    if (category === "All") return item;
+    return (item.category === category) ;
+    })
+   
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleCategory} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayItem.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
